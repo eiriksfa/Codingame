af7c7328094d6348c336025ff0ca298030957a6f
Eirik Fagerhaug <eirik.fagerhaug@gmail.com>
2017-12-02 17:59:41 +0100

    push

--- TheBridge.py
+++ TheBridge.py
@@ -5,8 +5,8 @@ import time
 # Auto-generated code below aims at helping you parse
 # the standard input according to the problem statement.
 
-m = int(input())  # the amount of motorbikes to control
-v = int(input())  # the minimum amount of motorbikes that must survive
+nbikes = int(input())  # the amount of motorbikes to control
+nsurvive = int(input())  # the minimum amount of motorbikes that must survive
 l0 = input()  # L0 to L3 are lanes of the road. A dot character . represents a safe space, a zero 0 represents a hole in the road.
 l1 = input()
 l2 = input()
@@ -33,12 +33,13 @@ def update_state(lanes, s, states, move):
         s += 1
     elif move == 'SLOW':
         s -= 1
+    if s < 0:
+        s = 0
     nstates = []
     for (x, y, a) in states:
         if a == 0:
             continue
         x += s
-        # TODO: What if limited by bridge rail?
 
         if move == 'UP':
             y -= 1
@@ -49,7 +50,7 @@ def update_state(lanes, s, states, move):
             if check_death(lanes, s, y, x) or check_death(lanes, s-1, y-1, x-1):
                 a = 0
         elif move == 'JUMP':
-            if lanes[y][x] == '0':
+            if x < len(lanes) or lanes[y][x] == '0':
                 a = 0
         else:
             if check_death(lanes, s, y, x):
@@ -58,7 +59,7 @@ def update_state(lanes, s, states, move):
     return s, nstates
 
 
-def check_status(lanes, states, v, m):
+def check_status(lanes, states, nsurvive, nbikes):
     dc = 0
     dx = 0
     for (x, y, a) in states:
@@ -66,31 +67,65 @@ def check_status(lanes, states, v, m):
             dc += 1
         else:
             dx = x
-    if m - dc < v:
+    if nbikes - dc < nsurvive:
         return -1
     if dx >= len(lanes[0]):
         return 1
     return 0
 
 
+def check_bounds(states, move, s):
+    if move == 'SLOW' and s < 1:
+        return False
+    if not (move == 'UP' or move == 'DOWN'):
+        return True
+    for (_, y, a) in states:
+        if move == 'UP' and y == 0 and a == 1:
+            return False
+        if move == 'DOWN' and y == 3 and a == 1:
+            return False
+    return True
+
+
+def rec_search(lanes, s, states, move, nsurvive, nbikes, depth):
+    ns, nstates = update_state(lanes, s, states, move)
+    nstatus = check_status(lanes, nstates, nsurvive, nbikes)
+    if nstatus == -1 or nstatus == 1:
+        return nstatus
+    if depth > 30:
+        return -1
+    for move in moves:
+        if not check_bounds(nstates, move, ns):
+            continue
+        nstatus = rec_search(lanes, ns, nstates, move, nsurvive, nbikes, depth+1)
+        if nstatus == 1:
+            return nstatus
+
+
+def search(lanes, s, states, nsurvive, nbikes):
+    for move in moves:
+        if not check_bounds(states, move, s):
+            continue
+        nstatus = rec_search(lanes, s, states, move, nsurvive, nbikes, 0)
+        if nstatus == 1:
+            return move
+
+
 # game loop
 while True:
     s = int(input())  # the motorbikes' speed
     states = []
-    for i in range(m):
+    for i in range(nbikes):
         # x: x coordinate of the motorbike
         # y: y coordinate of the motorbike
         # a: indicates whether the motorbike is activated "1" or destroyed "0"
         x, y, a = [int(j) for j in input().split()]
         states.append((x, y, a))
 
-    ns, nstate = update_state(lanes, s, states, 'SPEED')
-    nstatus = check_status(lanes, nstate, v, m)
-
-    print(nstatus, file=sys.stderr)
+    move = search(lanes, s, states, nsurvive, nbikes)
 
     # Write an action using print
     # To debug: print("Debug messages...", file=sys.stderr)
 
     # A single line containing one of 6 keywords: SPEED, SLOW, JUMP, WAIT, UP, DOWN.
-    print('SPEED')
+    print(move)
---
+++ defiib.py
@@ -0,0 +1,9 @@
+
+
+class Test:
+
+    def __init__(self):
+        pass
+
+    def test(self):
+        pass
